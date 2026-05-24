6c9d8e4785d18c4a68a1dbd43b97b4f1e242f1f2
Mads Asp <aspmads2@hotmail.com>
2026-05-24 19:26:24 +0200

    Tighten rating summary spacing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const GENRES = [
 
 const FEATURED_ROTATION_MS = 8000;
 const INITIAL_VISIBLE_COUNT = 48;
-const DATA_VERSION = "2026-05-24-20";
+const DATA_VERSION = "2026-05-24-21";
 const EXPANDED_LIST_STORAGE_KEY = "podcast-ratings-expanded-list";
 const NEW_BADGE_DAYS = 14;
 const SUPABASE_CONFIG = window.PODCAST_SUPABASE_CONFIG || {
